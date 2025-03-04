53bbfca7842fa8177f2b5b0d9502cbbae552263c
aysunfarhadli <aysunfarhadli4@gmail.com>
2025-03-04 13:12:53 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,18 +4,16 @@ class Program
 {
     static void Main()
     {
-        // İstifadəçidən iki ədəd daxil etməsini istəyirik
-        Console.Write("Birinci ədədi daxil edin: ");
+        Console.Write("Birinci ədədi daxil edin");
         int a = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("İkinci ədədi daxil edin: ");
+        Console.Write("İkinci ədədi daxil edin");
         int b = Convert.ToInt32(Console.ReadLine());
 
-        // Hər iki ədədin 2-yə tam bölünüb-bölünmədiyini yoxlayırıq
         if (a % 2 == 0 && b % 2 == 0)
         {
             int cem = a + b;
-            Console.WriteLine("Ədədlərin cəmi: " + cem);
+            Console.WriteLine(cem);
         }
         else
         {
